37b4ad93c0597c116870ce78a4f65cea2885a0b0
JohnGuilding <jwguilding@gmail.com>
2024-05-22 18:09:08 +0100

    Add deploy script

---
+++ accounts/safe7579/script/DeployRecovery.s.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {ZkEmailRecovery} from "src/zkEmailRecovery/ZkEmailRecovery.sol";
+import {SafeRecoveryModule} from "src/zkEmailRecovery/modules/SafeRecoveryModule.sol";
+
+contract DeployRecoveryScript is Script {
+    function run() public {
+        bytes32 salt = bytes32(uint256(0));
+
+        address verifier = 0xEdC642bbaD91E21cCE6cd436Fdc6F040FD0fF998;
+        address ecdsaOwnedDkimRegistry = 0xC83256CCf7B94d310e49edA05077899ca036eb78;
+        address emailAuthImpl = 0x1C76Aa365c17B40c7E944DcCdE4dC6e6D2A7b748;
+
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+
+        ZkEmailRecovery zkEmailRecovery = new ZkEmailRecovery{salt: salt}(
+            verifier,
+            ecdsaOwnedDkimRegistry,
+            emailAuthImpl
+        );
+        new SafeRecoveryModule{salt: salt}(address(zkEmailRecovery));
+
+        vm.stopBroadcast();
+    }
+}
